5ee3f5c58791e369ff30a8338b3669e83a0afa93
prosh14 <joshuasilva414@gmail.com>
2022-11-01 11:33:00 -0500

    empty commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 };
 
 export default MyApp;
