1ed8c6cb3897d9eafcb239fbecd8c089a73a13c2
Yash Verma <its_yash@icloud.com>
2023-11-09 14:44:48 -0600

    Added a list of officers and updated the project names and their logos.

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -12,7 +12,7 @@ export const ProjectItem: FunctionComponent<ProjectItemProps> = ({
 	description,
 }) => {
 	return (
-		<div className="h-[325px] w-full rounded-xl border-2 border-rc-darkblue p-[2px] text-rc-darkblue transition ease-in-out hover:bg-rc-darkblue hover:text-rc-yellow">
+		<div className="h-[325px] w-full rounded-xl border-2 border-rc-darkblue p-2 text-rc-darkblue transition ease-in-out hover:bg-rc-darkblue hover:text-rc-yellow">
 			<div className="flex h-[60%] items-center justify-center">{children}</div>
 			<div className="flex h-[40%] flex-col justify-center">
 				<h1 className="text-4xl font-bold">{title}</h1>
